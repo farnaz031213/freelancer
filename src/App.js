e4f1fa7ad5b7a280c98af7da3b82e503408ee48c
Farnaz Tavakol <tavakolfarnaz@gmail.com>
2019-11-22 13:17:44 +1100

    set up complete

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import logo from './logo.svg';
+import SignIn from './components/signUp/'
+import './App.css';
+
+function App() {
+  return (
+    <SignIn></SignIn>
+  );
+}
+
+export default App;
